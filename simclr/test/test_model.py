613517bfcac3f0c1bd9ebc2606712bf3773bd071
Alessio Piroli <alessiopiroli1@gmail.com>
2026-05-04 21:50:23 +0200

    formatting

--- simclr/test/test_model.py
+++ simclr/test/test_model.py
@@ -4,9 +4,11 @@ import torch
 from simclr.model.simclr_model import SimCLR
 from simclr.utils.misc import load_config
 
+
 @pytest.fixture
 def cfg():
     return load_config("simclr/config/simclr_config.yml")
+
 
 @pytest.mark.parametrize("bs, n_ch, h, w", [(1, 3, 224, 224)])
 def test_model(cfg, bs, n_ch, h, w):
