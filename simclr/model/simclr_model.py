b9fae553c53a2cbeb205aee9c4ecfa051a79a477
Alessio Piroli <alessiopiroli1@gmail.com>
2026-05-12 08:35:26 +0200

    updated model with simpler backbone

--- simclr/model/simclr_model.py
+++ simclr/model/simclr_model.py
@@ -2,28 +2,32 @@ import torch.nn as nn
 import torchvision.models as models
 
 
-class ResNet18BackBone(nn.Module):
+class Backbone(nn.Module):
     def __init__(self):
         super().__init__()
-        self.backbone = models.resnet18(weights=None)
-        self.backbone.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.backbone.maxpool = nn.Identity()
-        self.backbone.fc = nn.Identity()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
 
     def forward(self, x):
-        x = self.backbone(x)
+        x = self.model(x).flatten(1)
 
         return x
 
 
 class ProjHead(nn.Module):
-    def __init__(self, cfg):
+    def __init__(self):
         super().__init__()
-        self.cfg = cfg
-        self.dim1 = int(self.cfg.MODEL.HEAD.dim1)
-        self.dim2 = int(self.cfg.MODEL.HEAD.dim2)
+        self.dim1 = 512
+        self.dim2 = 128
         self.model = nn.Sequential(
-            nn.Linear(self.dim1, self.dim1), nn.BatchNorm1d(self.dim1), nn.ReLU(), nn.Linear(self.dim1, self.dim2)
+            nn.Linear(1152, self.dim1), nn.BatchNorm1d(self.dim1), nn.ReLU(), nn.Linear(self.dim1, self.dim2)
         )
 
     def forward(self, x):
@@ -33,12 +37,11 @@ class ProjHead(nn.Module):
 
 
 class SimCLR(nn.Module):
-    def __init__(self, cfg):
+    def __init__(self):
         super().__init__()
-        self.cfg = cfg
 
-        self.backbone = ResNet18BackBone()
-        self.head = ProjHead(self.cfg)
+        self.backbone = Backbone()
+        self.head = ProjHead()
 
     def forward(self, x):
         backbone_out = self.backbone(x)
