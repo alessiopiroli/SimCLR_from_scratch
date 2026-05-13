f0b81571321ff1ca24edc834886df8578b0c1cd2
Alessio Piroli <alessiopiroli1@gmail.com>
2026-05-13 08:53:12 +0200

    updated model

--- simclr/model/simclr_model.py
+++ simclr/model/simclr_model.py
@@ -1,50 +1,74 @@
 import torch.nn as nn
 import torchvision.models as models
 
-
-class Backbone(nn.Module):
-    def __init__(self):
+class Conv(nn.Module):
+    def __init__(self, in_ch, out_ch, k_size, stride, padding):
         super().__init__()
 
         self.model = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2)
-        )
-
-    def forward(self, x):
-        x = self.model(x).flatten(1)
-
-        return x
-
-
-class ProjHead(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.dim1 = 512
-        self.dim2 = 128
-        self.model = nn.Sequential(
-            nn.Linear(1152, self.dim1), nn.BatchNorm1d(self.dim1), nn.ReLU(), nn.Linear(self.dim1, self.dim2)
+            nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=k_size, stride=stride, padding=padding),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU()
         )
 
     def forward(self, x):
         x = self.model(x)
-
         return x
 
 
-class SimCLR(nn.Module):
+class SimCLREncoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.backbone = Backbone()
-        self.head = ProjHead()
+        self.model = nn.Sequential(
+            Conv(3, 32, 3, 2, 1),
+            Conv(32, 64, 3, 2, 1),
+            Conv(64, 128, 3, 2, 1),
+            Conv(128, 256, 3, 2, 1),
+            nn.MaxPool2d(2),
+            nn.Flatten(1, -1)
+        )
 
     def forward(self, x):
-        backbone_out = self.backbone(x)
-        head_out = self.head(backbone_out)
+        x = self.model(x)
+        return x
 
-        return head_out
+class SimCLRProjHead(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(256, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Linear(256, 128)
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+class SimCLRModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.encoder = SimCLREncoder()
+        self.head = SimCLRProjHead()
+
+    def forward(self, x, encoder_out=False):
+        h = self.encoder(x)
+
+        if encoder_out:
+            return h
+        
+        z = self.head(h)
+        
+        return z
+
+if __name__ == "__main__":
+    import torch
+    x = torch.randn([1, 3, 32, 32])
+    model = SimCLRModel().eval()
+    out = model(x, encoder_out=True)
+
+    print(f"Model output shape: {out.shape}")
